16995138d7421f675d3bedcac7629183bb335004
dyljz12 <dyljz12@163.com>
2018-09-18 20:35:56 +0800

    2

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ optdic = {'1': schoolmanage.schoolcontrol, '2': teachermanage.teachercontrol, '3
           '4': exit}
 
 while not programexitflag:
-    idcheck = input("Who are you?\n1:Administrator\n2:Teacher\n3:Student\n4:exit\n")
+    idcheck = input("Who areeee you?\n1:Administrator\n2:Teacher\n3:Student\n4:exit\n")
     if idcheck in optdic.keys():
         optdic[idcheck]()
+###报名后信息不在数据库里++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
