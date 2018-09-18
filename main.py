f5b5d88a2a4907e18764ac158181995ba83b18fd
dyljz12 <dyljz12@163.com>
2018-09-18 20:33:42 +0800

    2

--- main.py
+++ main.py
@@ -10,4 +10,3 @@ while not programexitflag:
     idcheck = input("Who are you?\n1:Administrator\n2:Teacher\n3:Student\n4:exit\n")
     if idcheck in optdic.keys():
         optdic[idcheck]()
-###报名后信息不在数据库里
